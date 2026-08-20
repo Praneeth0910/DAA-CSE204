954913d2ba065fecd53dae9eda4ff03db4f73c27
praneeth0910 <praneeth.gupta09@gmail.com>
2026-08-20 21:18:33 +0530

    Implementation of tower of henoi in iterative approach

---
+++ Lab-3/Source_code/henoi2.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+struct Task{
+	bool isMove;
+	int n;
+	char src, dest, aux;
+};
+
+void hanoiIterative(int totalDisks, char source, char destination, char auxiliary){
+	stack<Task> s;
+	s.push({false, totalDisks, source, destination, auxiliary});
+	while(!s.empty()){
+		Task current = s.top();
+		s.pop();
+		if(current.isMove) {
+			cout<<"Move disk"<<current.n<<"from"<<current.src<<"to"<<current.dest<<endl;
+		}else{
+			if(current.n==1){
+				cout<<"Move disk"<<current.n<<"from"<<current.src<<"to"<<current.dest<<endl;
+			} else {
+           		 // Base case: only one disk, print directly
+	           		 if (current.n == 1) {
+        	       			 cout << "Move disk 1 from " << current.src << " to " << current.dest << endl;
+           			 } else {
+               				 // Push tasks to the stack in REVERSE order of execution
+                
+               				 // 3. Move n-1 disks from auxiliary to destination
+               				 s.push({false, current.n - 1, current.aux, current.dest, current.src});
+                
+			                // 2. Move the largest disk from source to destination
+               				 s.push({true, current.n, current.src, current.dest, current.aux});
+                
+			                // 1. Move n-1 disks from source to auxiliary
+               				 s.push({false, current.n - 1, current.src, current.aux, current.dest});
+           			 }
+       			 }
+   		 }
+	}
+}
+int main() {
+    cout << "\n--- Iterative (Stack) Towers of Hanoi ---" << endl;
+    hanoiIterative(3, 'A', 'C', 'B');
+    return 0;
+}
